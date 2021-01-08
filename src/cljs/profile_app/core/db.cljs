76484497fed18868a0f36864d283fd4b542ba810
Karina <k@rina.wtf>
2021-01-08 05:42:34 +0300

    :card_file_box: Change default db state

--- src/cljs/profile_app/core/db.cljs
+++ src/cljs/profile_app/core/db.cljs
@@ -1,10 +1,12 @@
 (ns profile-app.core.db)
 
 (def default-db
-  {:firstname "Karina"
+  {:active-route :showcase
+
+   :firstname "Karina"
    :secondname "Drummer"
 
-   :bio ["( 🦊 👩🏻‍💻 🏳️‍🌈 💞 )"
+   :bio ["( 🦊 👩🏻‍💻 🏳️‍🌈 )"
          "Totally fox-obsessed polymath"
          "Working in software development"
          "The owner of https://t.me/TheFoxNews"]})
